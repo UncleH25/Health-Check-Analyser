e2f3c326f09ec3aaba2024a84c7d0a84a6439a0d
UncleH25 <oneilljarryd@gmail.com>
2025-05-12 18:40:53 +0200

    Added code to prompt the user to input their age, temperature, and gender and and take in the input and store it in their respective variable.

--- Health Check Analyser.cpp
+++ Health Check Analyser.cpp
@@ -2,10 +2,24 @@
 #include <iomanip>
 #include <string>
 
+using namespace std;
+
 int main()
 {
 	//Variables
 	int age;
 	double temperature;
 	char gender;
+
+	//Input
+	cout << "Enter your age: ";
+	cin >> age;
+	cout << "Enter your temperature: ";
+	cin >> temperature;
+
+	//Clear the input buffer
+	cin.ignore();
+
+	cout << "Enter your gender (M/F): ";
+	cin.get(gender);
 }
